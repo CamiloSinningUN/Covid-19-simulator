d04899bfa9adb4cae44fb8d4ba451bc17742fffb
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2022-01-09 21:59:59 -0500

    names changed

--- Covid-19-simulator/src/Main/Graficar.java
+++ Covid-19-simulator/src/Main/Graficar.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lab02_juanjulio_jorgesalazar_camilosinning;
+package Main;
 
 import java.awt.Color;
 import java.awt.Dimension;
